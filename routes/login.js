6d137f0d40752527c81a8fdccea07fd5fe8205dd
Prashanth Swargam <swargam.prashanth@gmail.com>
2018-09-20 02:23:53 -0400

    enhancing  login

--- routes/login.js
+++ routes/login.js
@@ -2,15 +2,35 @@ var express = require('express');
 var router = express.Router();
 var request = require('request')
 var bodyParser = require('body-parser')
-
+var axios = require('axios')
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  console.log(req.body.passwd);
+  console.log("here")
+  var reqBody=req.body.username
+  console.log(reqBody)
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.send(req.body)
+  
+  axios.post('http://192.168.0.12:8888/auth/users/logmein',
+  
+  {
+    username : req.body.username,
+    password : req.body.password
+  }).then(result=>{
+      res.send(result)
+  }).catch(error=>{
+    res.status('404').send('not found')
+  });
+  // request.post({
+  //   headers: {'content-type' : 'application/x-www-form-urlencoded'},
+  //   url:     'http://192.168.0.12/auth/users/logmein',
+  //   body: {"username":"pswargam",
+  //           "password":"asdfjkl;'"}
+  // }, function(error, response, body){
+  //   console.log(body);
+  // });
+  
   
 })
 module.exports = router;
