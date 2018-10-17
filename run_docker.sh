cd752c026a387812a60b917fda01f88419ec8745
Nawaz <k.nawaz.h@gmail.com>
2018-10-17 15:12:14 -0400

    changed jenkins

--- run_docker.sh
+++ run_docker.sh
@@ -9,7 +9,7 @@ docker rmi nawazkh/maven:3.5.4
 echo "---------removed old containers and images if existed---------"
 docker build -f Dockerfile -t nawazkh/maven:3.5.4 .
 echo "---------built docker image for maven ---------"
-cd restful-user-authentication
+cd restful-user-authentication/
 
 docker run -it --rm -v "$PWD":/app -w /app --network host \
 -p 8888:8888 \
