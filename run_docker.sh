3df6fa694c4179bdd16de0ad19a1b764dcbd8fa4
Nawaz <k.nawaz.h@gmail.com>
2018-10-17 16:56:25 -0400

    changed jenkins

--- run_docker.sh
+++ run_docker.sh
@@ -4,25 +4,25 @@
 #docker swarm leave --force
 
 docker rm -f login_authenticaion_container
-#docker rmi nawazkh/maven:3.5.4
-#docker rmi nawazkh/login:latest
+docker rmi nawazkh/maven:3.5.4
+docker rmi nawazkh/login:latest
 cd restful-user-authentication/
 echo "---------removed old containers and images if existed---------"
-#docker build -f Dockerfile.maven -t nawazkh/maven:3.5.4 .
+docker build -f Dockerfile.maven -t nawazkh/maven:3.5.4 .
 echo "---------built docker image for maven ---------"
-#docker run --rm -v "$PWD":/app -w /app --network host -p 8888:8888 nawazkh/maven:3.5.4 clean install
+docker run --rm -v "$PWD":/app -w /app --network host -p 8888:8888 nawazkh/maven:3.5.4 clean install
 echo "--------- created the jar for the project ---------"
+#---------#
 #docker run -d -it -v "$PWD":/app -w /app --network host \
 #--name login_authenticaion_container -p 8888:8888 \
 #nawazkh/maven:3.5.4 \
 #java -jar target/restful-user-authentication-0.0.1-SNAPSHOT.jar
-#docker build -f Dockerfile -t nawazkh/login:latest .
+#---------#
+docker build -f Dockerfile -t nawazkh/login:latest .
 docker-compose up -d
-docker container ls
-docker image ls
-ls -l
-docker container ls
 echo "--------- login module up ---------"
+#---------#
 #cd ..
 #docker swarm init
 #docker stack deploy -c docker-compose.yml login_authenticaion_service
+#---------#
