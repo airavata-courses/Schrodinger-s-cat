d0de30c9420b09c777e463d63aa36f81f3457411
Nawaz <k.nawaz.h@gmail.com>
2018-10-24 00:48:49 -0400

    another code

---
+++ rabbitmq.smtp.server/src/main/java/com/foodcode/microservice/rabbitmq/smtp/server/App.java
@@ -0,0 +1,13 @@
+package com.foodcode.microservice.rabbitmq.smtp.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
