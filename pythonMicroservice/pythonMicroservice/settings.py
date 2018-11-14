982e966d428e2972a76bc13900efd5c19858d31f
Prashanth Swargam <swargam.prashanth@gmail.com>
2018-11-14 02:02:19 -0500

    Adding postgres configuration

--- pythonMicroservice/pythonMicroservice/settings.py
+++ pythonMicroservice/pythonMicroservice/settings.py
@@ -74,14 +74,23 @@ WSGI_APPLICATION = 'pythonMicroservice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db1',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
