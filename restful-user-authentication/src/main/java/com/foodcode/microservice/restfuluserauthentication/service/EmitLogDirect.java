e3e755926cfd3a6d723f60ddfa32c302c93dfd2f
Nawaz <k.nawaz.h@gmail.com>
2018-12-09 14:38:17 -0500

    changing service names for login

--- restful-user-authentication/src/main/java/com/foodcode/microservice/restfuluserauthentication/service/EmitLogDirect.java
+++ restful-user-authentication/src/main/java/com/foodcode/microservice/restfuluserauthentication/service/EmitLogDirect.java
@@ -24,7 +24,7 @@ public class EmitLogDirect {
 		try {
 			ConnectionFactory factory = new ConnectionFactory();
 //			factory.setHost("rabbitserver");//host name
-			factory.setHost("service-rabbitserver");//host name
+			factory.setHost("service-smtp-rabbitmq");//host name
 			//factory.setPort(5672);
 			Connection connection = factory.newConnection();
 			Channel channel = connection.createChannel();
