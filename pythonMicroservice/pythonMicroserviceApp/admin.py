3c154dcceb40f5ba9c4f50583b0c733b0f4ebb89
Prashanth Swargam <swargam.prashanth@gmail.com>
2018-12-09 16:21:31 -0500

    Removing irrelevant code

--- pythonMicroservice/pythonMicroserviceApp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Recipe,Ingredient
-
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
--- pythonMicroservice/pythonMicroserviceApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PythonmicroserviceappConfig(AppConfig):
-    name = 'pythonMicroserviceApp'
